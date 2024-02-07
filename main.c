0a19a0b24c691497d1471e6f5bd849928a8aec4f
Tia Self <tiamarie.self@bsu.edu>
2024-02-07 15:55:58 -0500

    remove unused variables

--- main.c
+++ main.c
@@ -4,7 +4,6 @@
 
 #define FN_LENGTH 100
 #define MAX_LENGTH 1000
-#define STACK_LIMIT 10
 #define SIZE 4  
 int top = -1, stack[SIZE];  
 
@@ -30,14 +29,12 @@ void pop()
     {   
         top = top - 1;  
     }  
-}  
+} 
 
 int main () {
     FILE *file;
     char filename[FN_LENGTH];
     char line[MAX_LENGTH]; 
-    int stack[STACK_LIMIT];
-
 
     printf("This filereader will read a file and check for balanced parentheses.\n");
     printf("Enter the name of the file you want to read: ");
@@ -65,7 +62,7 @@ int main () {
             } else if (line[i] == '}') {
                 pop();
                 printf("curly brace removed from stack\n");
-            }
+            } 
 
             if (line[i] == '[') {  
                 push(line[i]);
