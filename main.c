ef35a12ab4c599fecfcceecf85af8f20ae6229d5
Tia Self <tiamarie.self@bsu.edu>
2024-02-07 22:42:58 -0500

    condense code that checks for matches, print out appropriate error messages

--- main.c
+++ main.c
@@ -5,39 +5,46 @@
 #define FN_LENGTH 100
 #define MAX_LENGTH 1000
 #define SIZE 4  
-int top = -1, stack[SIZE];  
 
-void push(char x)  
-{  
-    if (top == SIZE - 1)  
-    {  
+int top = -1, stack[SIZE]; 
+char filename[FN_LENGTH];
+char line[MAX_LENGTH]; 
+FILE *file;
+
+void push(char item) {  
+    if (top == SIZE - 1)  {  
         printf("\nOverflow");  
     }  
-    else  
-    {  
+    else {  
         top = top + 1;  
-        stack[top] = x;  
+        stack[top] = item;  
     }  
 }  
-void pop()  
-{  
-    if (top == -1)  
-    {  
+
+char pop() {  
+    if (top == -1) {  
         printf("\nUnderflow");  
     }  
-    else  
-    {   
-        top = top - 1;  
+    else {  
+        top = top - 1;
     }  
 } 
 
-int main () {
-    FILE *file;
-    char filename[FN_LENGTH];
-    char line[MAX_LENGTH]; 
+int isPair(char a, char b) {
+    if (a == '(' && b == ')') {
+        return 1;
+    } else if (a == '{' && b == '}') {
+        return 1;
+    } else if (a == '[' && b == ']') {
+        return 1;
+    } else {
+        return 0;
+    }
+}
 
-    printf("This filereader will read a file and check for balanced parentheses.\n");
-    printf("Enter the name of the file you want to read: ");
+int main () {
+
+    printf("\nThis program will read a file and check for balanced parentheses.\nEnter the name of the file you want to read: ");
     scanf("%s", filename);
 
     file = fopen(filename, "r");
@@ -48,43 +55,26 @@ int main () {
 
     while (fgets(line, sizeof(line), file) != NULL) {
         for (size_t i = 0; i < strlen(line); i++) { 
-            if (line[i] == '(') {  
-                push(line[i]);
-                printf("parenthesis added to stack\n");
-            } else if (line[i] == ')') {
-                pop();
-                printf("parenthesis removedfrom stack\n");
-            }
 
-            if (line[i] == '{') {  
-                push(line[i]);
-                printf("curly brace added to stack\n");
-            } else if (line[i] == '}') {
-                pop();
-                printf("curly brace removed from stack\n");
-            } 
-
-            if (line[i] == '[') {  
-                push(line[i]);
-                printf("bracket added to stack\n");
-            } else if (line[i] == ']') {
-                pop();
-                printf("bracket removed from stack\n");
+            if (line[i] == '(' || line[i] == '{' || line[i] == '[') {  
+                push(line[i]); 
+            } else if (line[i] == ')' || line[i] == '}' || line[i] == ']') {
+                if (top == -1) { 
+                    printf("\nSyntax errpr: No opening bracket at line x\n");
+                } else if (!isPair(stack[top], line[i])){
+                    printf("\nSyntax error: No closing bracket at line x\n");
+                    pop();
+                } else {
+                    pop();
+                }
             }
         }
     }
 
-    /*
-    TODO:
-    1. print out line where mismatch occured
-    2. figure out how to ignore (), [], and {} in comments
-    */ 
+    if (top == -1) {
+        printf("\nAll (), {}, and [] are balanced.\n");
+    }
 
     fclose(file);
 
-    if (top == -1) {
-        printf("all matched");
-    } else {
-        printf("mismatch");
-    }
 }
