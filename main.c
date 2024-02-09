b6da889bd3cbb3673ac6f589a3a6ffe648d785be
Tia Self <tiamarie.self@bsu.edu>
2024-02-09 16:32:43 -0500

    prints out error at correct line now!!!!!!!!!!!!!!!!!!!!!!!!!!!

--- main.c
+++ main.c
@@ -44,6 +44,8 @@ int isPair(char a, char b) {
 
 int main () {
 
+    int line_counter = 1;
+
     printf("\nThis program will read a file and check for balanced (), {}, and [].\nEnter the name of the file you want to check: ");
     scanf("%s", filename);
 
@@ -60,24 +62,23 @@ int main () {
             do... if i'm being honest i looked this up :D, but he showed it a different way in class
             i think, so it can definitely be changed. 
             */
-
             if (line[i] == '(' || line[i] == '{' || line[i] == '[') {  
                 push(line[i]); // if it finds an open bracket it adds the char to the stack
             } else if (line[i] == ')' || line[i] == '}' || line[i] == ']') {
                 if (top == -1) { // so if the stack is still empty and it found a closing bracket, then that's a problem
-                    printf("\nSyntax error: No opening bracket at line %i.\n", line[i]); 
+                    printf("\nSyntax error: No opening bracket at line %i.\n", line_counter); 
                     /*
                     using line[i] is wrong :( it's showing the place in the characters where the error is, but using i also 
                     doesn't work. not sure what to use to get the correct line number????*/
                 } else if (!isPair(stack[top], line[i])){ //checks to see if it matches the last char that got added to the stack
-                    printf("\nSyntax error: No closing bracket at line %i.\n", line[i]);
+                    printf("\nSyntax error: No closing bracket at line %i.\n", line_counter);
                     pop(); // remove char from the stack since it's already been checked
                 } else {
                     pop(); // still remove the char from the stack since the right closing bracket was found
                 }
             }
-
         }
+        line_counter++;
     }
 
     if (top == -1) { // stack is empty if it's -1 and if the stack is empty then all of the brackets had a match
